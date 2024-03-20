0eb717518b0afd0310bdc948dcb81fb913dc8e54
Your Name <you@example.com>
2024-03-20 17:38:17 -0600

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742, -0.120850),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
