3411f537c0b43ad21b2efb4a50077bcacb7c09db
Your Name <you@example.com>
2024-03-21 19:30:38 -0600

    sd

---
+++ app copy.js
@@ -0,0 +1,32 @@
+const x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+// function showPosition(position) {
+//   x.innerHTML = "Passcode: " + position.coords.latitude +
+//     "<br>Passphrase: " + position.coords.longitude;
+//   localStorage.setItem('myList', JSON.stringify(x.innerHTML))
+// }
+
+function showPosition(position) {
+  x.innerHTML = "Passcode: " + position.coords.latitude +
+    "<br>Passphrase: " + position.coords.longitude;
+  localStorage.setItem('myList', JSON.stringify(x.innerHTML))
+}
+
+
+
+const myElement = document.getElementById("testBtn");
+myElement.innerHTML = "Clicked";
+
+
+
+
+
+
---
+++ root/app.js
@@ -0,0 +1,49 @@
+
+
+const x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+// function showPosition(position) {
+//   x.innerHTML = "Passcode: " + position.coords.latitude +
+//     "<br>Passphrase: " + position.coords.longitude;
+//   localStorage.setItem('myList', JSON.stringify(x.innerHTML))
+// }
+
+
+const showPosition = (position) => {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+  localStorage.setItem('myDataStorage', JSON.stringify(x.innerHTML))
+
+}
+
+const button = document.getElementById("btn")
+button.addEventListener("click", () => {
+  getLocation();
+
+})
+// const myValue = 
+
+var myData = localStorage.getItem('myDataStorage');
+const userInput = document.getElementById('input').value = myData;
+
+// const userInput = document.getElementById('input').value =
+
+
+
+
+
+
+
+
+
+
+
+
